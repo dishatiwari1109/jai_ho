eeadf0bcb2975d9d559b1134c2f53d2f5f6aa8bc
harshitasalvi-sa <harshita.salvi@startappss.com>
2026-01-14 15:34:33 +0530

    Called jatin's componenent to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Harshita from "./components/Harshita";
 import Ishika from "./components/Ishika";
+import Jatin from "./components/jatin/WeatherApi"
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/ishika" element={<Ishika />} />
-        <Route path="/jatin" element={<h2>Jatin Page</h2>} />
+        <Route path="/jatin" element={<Jatin/>} />
         <Route path="/harshita" element={<Harshita />} />
         <Route path="/disha" element={<h2>Disha Page</h2>} />
       </Routes>
