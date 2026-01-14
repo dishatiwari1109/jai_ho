80b42968d3f298fab753003c8c79efc0e59ab308
Ishika Bansiwal <ishikabansiwal411@gmail.com>
2026-01-14 14:58:58 +0530

    firstt

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Harshita from "./components/Harshita";
+import Ishika from "./components/Ishika";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/ishika" element={<h2>Ishika Page</h2>} />
+        <Route path="/ishika" element={<Ishika />} />
         <Route path="/jatin" element={<h2>Jatin Page</h2>} />
-        <Route path="/harshita" element={<Harshita/>} />
+        <Route path="/harshita" element={<Harshita />} />
         <Route path="/disha" element={<h2>Disha Page</h2>} />
       </Routes>
     </BrowserRouter>
