6c46acf39bdde3db6dc7c2a804635bb3581a0b94
Ishika Bansiwal <ishikabansiwal411@gmail.com>
2026-01-16 12:42:33 +0530

    Second Commit

--- src/components/Ishika.jsx
+++ src/components/Ishika.jsx
@@ -4,6 +4,9 @@ const Ishika = () => {
   return (
     <div>
       <h1>This is Ishika's Page</h1>
+      <p>Welcome Ishika's Profile</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ipsum tenetur
+         eius eveniet pariatur laudantium maiores ducimus nihil neque explicabo.</p>
     </div>
   )
 }
